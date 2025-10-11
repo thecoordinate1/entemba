43f6fe591898b208a19f7d888799e329ed449a04
Mapalo Lungu <mapalolungu65@gmail.com>
2025-10-11 11:24:29 +0000

    Register a Service Worker

--- next.config.ts
+++ next.config.ts
@@ -5,7 +5,7 @@ const withPWA = require('next-pwa')({
   dest: 'public',
   register: true,
   skipWaiting: true,
-  disable: process.env.NODE_ENV === 'development',
+  disable: false, // Ensure PWA is enabled in development
 });
 
 const nextConfig: NextConfig = {
@@ -31,7 +31,7 @@ const nextConfig: NextConfig = {
         pathname: '/**',
       },
       {
-        protocol: 'https',
+        protocol: 'https' as const,
         hostname: 'noympgnaetcrtpdlkkry.supabase.co', // Added Supabase hostname
         port: '',
         pathname: '/**',
