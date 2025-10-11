fa590b29f2b1390f16a3eda50e3ff71599088e5a
Mapalo Lungu <mapalolungu65@gmail.com>
2025-10-11 11:51:23 +0000

    in the config file change this line disable: process.env.NODE_ENV === '

--- next.config.ts
+++ next.config.ts
@@ -3,9 +3,7 @@ import type {NextConfig} from 'next';
 
 const withPWA = require('next-pwa')({
   dest: 'public',
-  register: true,
-  skipWaiting: true,
-  disable: false, // Ensure PWA is enabled in development
+  disable: process.env.NODE_ENV === 'development',
 });
 
 const nextConfig: NextConfig = {
