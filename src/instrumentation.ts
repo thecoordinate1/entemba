79f6ce5ae0140d0ead25bc92dc3bdd83410ede20
bando <bandobix55555@gmail.com>
2025-10-23 09:56:53 +0000

    The app isn't starting. Please investigate what could be wrong based on

--- src/instrumentation.ts
+++ src/instrumentation.ts
@@ -1,13 +1,5 @@
-import * as Sentry from '@sentry/nextjs';
 
 export async function register() {
-  if (process.env.NEXT_RUNTIME === 'nodejs') {
-    await import('../sentry.server.config');
-  }
-
-  if (process.env.NEXT_RUNTIME === 'edge') {
-    await import('../sentry.edge.config');
-  }
+  // This file can be used to register instrumentation hooks.
+  // See https://nextjs.org/docs/app/building-your-application/optimizing/instrumentation
 }
-
-export const onRequestError = Sentry.captureRequestError;
